625925f2d02ec77e06f286458c3044fed44fcacf
Joe D'Amore <joe@joedamore.me>
2022-11-07 13:42:50 -0500

    Add setsmt source

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let smt_file = "/sys/devices/system/cpu/smt/control";
+    let args: Vec<String> = std::env::args().collect();
+
+    let smt_status = match args.get(1) {
+        Some(val) => val.clone(),
+        None => {
+            // Take no action if no status is provided.
+            std::process::exit(0);
+        }
+    };
+
+    match smt_status.as_str() {
+        "on" | "off" => {
+            std::fs::write(smt_file, smt_status).expect("Failed to write to CPU SMT file");
+        }
+        _ => {
+            println!("Unknown value '{}'", smt_status);
+        }
+    };
 }
